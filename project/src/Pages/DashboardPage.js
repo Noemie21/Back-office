142b4283e260c34daf94fe567d49ec2503398902
Noémie <noemie.kpatenon@supinternet.fr>
2022-03-22 16:28:51 +0100

    Dashboard page

--- project/src/Pages/DashboardPage.js
+++ project/src/Pages/DashboardPage.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import Menu from '../Components/Informations';
+import SearchBar from '../Components/SearchBar';
 import Left from '../Components/Dashboard/Left';
+import Deals from '../Components/Dashboard/Deals';
+import Tasks from '../Components/Dashboard/Tasks';
 
 export default function DashboardPage() {
   return (
-    <div className="bg-background">
-      <h1>HI !</h1>
-      <Left />
+    <div className="flex flex-row">
+      <Menu className="w-64" />
+      <div className="w-full">
+        <SearchBar />
+        <div className="bg-background p-9 flex flex-row justify-between">
+          <Left />
+          <div className="w-2/5 flex flex-col">
+            <Deals />
+            <Tasks />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
